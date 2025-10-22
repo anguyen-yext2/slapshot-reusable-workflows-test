9a7b6dd0fd4d1cf9f518e0d15a79e100a83d4150
anguyen-yext2 <anguyen@yext.com>
2025-10-22 12:45:44 -0400

    add working dir

--- run-script/index.js
+++ run-script/index.js
@@ -5,11 +5,12 @@ const { execSync } = require('child_process');
 try {
   const tmpDir = process.env.RUNNER_TEMP || os.tmpdir();
   const scriptPath = path.join(tmpDir, `script-${Date.now()}.sh`);
+  const workingDirectory = core.getInput('working_directory');
 
   fs.writeFileSync(scriptPath, `#!/bin/bash\n${script}`);
   fs.chmodSync(scriptPath, 0o755);
 
-  execSync(scriptPath, { stdio: 'inherit' });
+  execSync(`cd ${workingDirectory} && ${scriptPath}`, { stdio: 'inherit' });
   fs.unlinkSync(scriptPath);
 } catch (error) {
   core.setFailed(error.message);
