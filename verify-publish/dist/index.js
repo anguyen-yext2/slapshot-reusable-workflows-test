2d3c1b6c47ccc27b607191c5662429290d4498e7
anguyen-yext2 <anguyen@yext.com>
2025-11-03 13:24:32 -0500

    test

--- verify-publish/dist/index.js
+++ verify-publish/dist/index.js
@@ -2823,37 +2823,21 @@ const path = __nccwpck_require__(17);
 
 const commitMsg = core.getInput('COMMIT_MSG');
 const repoPath = core.getInput('REPO_PATH');
-
-let pkgPath = path.join(repoPath, 'package.json');
-let pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
+console.log('>>>repo path: ', repoPath)
+const pkgPath = path.join(repoPath, 'package.json');
+const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
 
 let { version: currentVersion, private: isPrivate } = pkg;
 
-let expectedCommitMsg = `release: v${currentVersion}`;
-// const expectedMonorepoCommitMsg = `release: ${pkg.name}@v${currentVersion}`;
-if (commitMsg !== expectedCommitMsg) {//} && commitMsg !== expectedMonorepoCommitMsg) {
-  try {
-    // handle monorepo
-    const githubTag = commitMsg.replace(/^release:\s*/, '').trim();
-    const versionIndex = githubTag.lastIndexOf('@v');
-    if (versionIndex === -1) {
-      core.setFailed('Unexpected commit message format for a package contained in a monorepo');
-    }
-    const expectedPackageName = githubTag.slice(0, versionIndex);
-    const expectedPackageVersion = githubTag.slice(versionIndex + 2);
-    pkgPath = path.join(repoPath, `packages/${expectedPackageName}`, 'package.json');
-    pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
-    expectedCommitMsg = `release: ${expectedPackageName}@v${currentVersion}`;
-    ({ version: currentVersion, private: isPrivate } = pkg);
-    if (currentVersion !== expectedPackageVersion) {
-      core.setFailed(`Invalid commit message. \nExpected: '${expectedCommitMsg}'.\nActual: '${commitMsg}'`);
-    } 
-  } catch (err) {
+const expectedCommitMsg = `release: v${currentVersion}`;
+if (commitMsg !== expectedCommitMsg) {
+  // handle monorepo
+  const packagesDir = path.resolve(repoPath, 'packages');
+  if (fs.existsSync(packagesDir)) {
+    currentVersion = getPackageVersionInMonorepo();
+  } else {
     core.setFailed(`Invalid commit message. \nExpected: '${expectedCommitMsg}'.\nActual: '${commitMsg}'`);
-    console.error('Error attempting to handle monorepo:\n', err);
-    // core.setFailed(`Failed to parse ${packageJsonPath}:`, err);
   }
-  // core.setFailed(`Invalid commit message.\nExpected: '${expectedCommitMsg}' or '${expectedMonorepoCommitMsg} if package lives under a monorepo'.\nActual: '${commitMsg}'`);
 } else if (isPrivate) {
   core.setFailed('Package is private.');
 }
