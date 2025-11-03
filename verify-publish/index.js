2dba0c40c097ce1886872ece5383dc06717ec7c6
anguyen-yext2 <anguyen@yext.com>
2025-11-03 16:08:59 -0500

    test

--- verify-publish/index.js
+++ verify-publish/index.js
@@ -27,7 +27,7 @@ function verifyPublish() {
     // attempt to treat the current package as a monorepo that contains the target package to be published
     const packagesDir = path.resolve(repoPath, 'packages');
     if (fs.existsSync(packagesDir)) {
-      return getPackageVersionInMonorepo(packagesDir);
+      return handleMonorepo(packagesDir);
     } else {
       core.setFailed(`Invalid commit message. \nExpected: '${expectedCommitMsg}'.\nActual: '${commitMsg}'`);
     }
