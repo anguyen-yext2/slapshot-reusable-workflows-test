35df55d53d5d8cbb048a9fb3d59868d71d7a08f5
anguyen-yext2 <anguyen@yext.com>
2025-11-03 13:32:49 -0500

    test

--- verify-publish/index.js
+++ verify-publish/index.js
@@ -15,7 +15,7 @@ if (commitMsg !== expectedCommitMsg) {
   // handle monorepo
   const packagesDir = path.resolve(repoPath, 'packages');
   if (fs.existsSync(packagesDir)) {
-    currentVersion = getPackageVersionInMonorepo();
+    currentVersion = getPackageVersionInMonorepo(packagesDir);
   } else {
     core.setFailed(`Invalid commit message. \nExpected: '${expectedCommitMsg}'.\nActual: '${commitMsg}'`);
   }
@@ -31,7 +31,7 @@ core.setOutput('npm_tag', currentVersion.includes('rc')
       ? 'alpha'
       : 'latest');
 
-function getPackageVersionInMonorepo() {
+function getPackageVersionInMonorepo(packagesDir: string) {
   const githubTag = commitMsg.replace(/^release:\s*/, '').trim();
 
   const versionIndex = githubTag.lastIndexOf('@v');
